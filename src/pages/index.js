d3e71edff8b5fab20be44f19aee1e4ade522fcbd
TrojanAttack <rbza98@outlook.com>
2021-02-10 19:37:30 +0600

    accout change and minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,13 @@ import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
-
       <Navbar toggle={toggle} />
     </>
   );
