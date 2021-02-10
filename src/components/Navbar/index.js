1f403b28332ba0da02895b5e0e9c55f2b93da5ce
TrojanAttack <rbza98@outlook.com>
2021-02-11 03:08:46 +0600

    transparent menu.... almost ....

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 import {
   Nav,
   NavLogo,
@@ -12,34 +13,50 @@ import {
   NavBtnLink,
 } from "./NavbarElements";
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">Rafath Auvee</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/">Rafath Auvee</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
 
-          <NavBtn>
-            <NavMenu>
-              <NavItem>
-                <NavLinks to="/about">About</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/projects">Projects</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/achievement">Achievement</NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/contact">Contact</NavLinks>
-              </NavItem>
-              <NavBtnLink to="/resume">Resume</NavBtnLink>
-            </NavMenu>
-          </NavBtn>
-        </NavbarContainer>
-      </Nav>
+            <NavBtn>
+              <NavMenu>
+                <NavItem>
+                  <NavLinks to="/about">About</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="/projects">Projects</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="/achievement">Achievement</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="/contact">Contact</NavLinks>
+                </NavItem>
+                <NavBtnLink to="/resume">Resume</NavBtnLink>
+              </NavMenu>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
