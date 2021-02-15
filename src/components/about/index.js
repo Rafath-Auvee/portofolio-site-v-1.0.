5a270776be08f3748aef71a4aec4e85802b6763b
TrojanAttack <rbza98@outlook.com>
2021-02-16 02:08:53 +0600

    about me section done

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,21 +1,33 @@
 import React from "react";
-import "./about.css";
+import "./style.css";
 
 const About = () => {
   return (
     <>
-      <div className="about">
-        <div className="bodyparts">
-          <div className="cursor"></div>
-          <div className="cursor-follower"></div>
-          <div className="wrapper">
-            <div className="portfolio-item">
-              <div className="portfolio-thumb">
-                <img
-                  src="http://www.csbofficial.com/public_html/team/image/1607261618.jpeg"
-                  alt="Auvee"
-                />
-              </div>
+      <div className="start">
+        <div className="container">
+          <div className="aboutme">
+            <div className="headwithline">
+              <h2>About Me</h2>
+              <div className="bre"></div>
+            </div>
+            <p className="para">
+              Hello! My name is Rafath Bin Zafar Auvee. An enthusiastic,
+              self-taught, motivated programmer I am studying Bachelor of
+              Science & Engineering in Computer Science and Engineering at
+              Bangladesh Army University of Science and Technology.
+              <br /> For me, computer programming is love, passion, exploration
+              & much more. Basically, I do a lot of self-made Arduino projects
+              and solve problems in online Judge.
+            </p>
+          </div>
+          <div className="hex">
+            <div className="shape">
+              <img
+                src="https://andrafath.files.wordpress.com/2021/02/20287127_880074228823588_6235573847450463520_o.jpg"
+                alt="Rafath Bin Zafar Auvee"
+              />
+              <div className="content"></div>
             </div>
           </div>
         </div>
