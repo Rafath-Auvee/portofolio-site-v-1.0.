36a5d3e96b8dc369e4ed640a63c0b8cc3cf5eb32
TrojanAttack <rbza98@outlook.com>
2021-02-18 23:26:43 +0600

    email button added. Also button style

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -10,6 +10,13 @@ const Contact = () => {
         Whether you have an idea for a project or just want to chat, <br />
         feel free to shoot me an email!
       </p>
+      <section class="buttons3">
+        <div class="container3">
+          <a href="rafath.auvee@gmail.com" class="btn btn-5">
+            Say Hello!
+          </a>
+        </div>
+      </section>
     </div>
   );
 };
